57bd42bedcf36f39b45756d7109fab11bdda6dd6
emelinelawford <72405155+emelinelawford@users.noreply.github.com>
2022-03-27 04:33:41 +0100

    product component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,84 @@
-import React from 'react';
-import { ChakraProvider, Box, Grid, Button, theme } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
+import React, { useState } from 'react';
+import {
+  ChakraProvider,
+  FormControl,
+  FormLabel,
+  Switch,
+  HStack,
+  VStack,
+} from '@chakra-ui/react';
 import Product from './components/product/product';
+
+import Outlook from './images/icons/outlook.svg';
+import OneDrive from './images/icons/onedrive.svg';
+import SharePoint from './images/icons/sharepoint.svg';
 import { MyNewTheme } from './styles/theme';
 
+const data = [
+  {
+    name: 'Outlook',
+    desc: 'Email & Calendar',
+    productValue: { current: '20', target: '30' },
+    logo: Outlook,
+    color1: '#fed243',
+    color2: '#e7b008',
+  },
+  {
+    name: 'OneDrive',
+    desc: 'File Storage',
+    productValue: { current: '10', target: '35' },
+    logo: OneDrive,
+    color1: '#c0f03c',
+    color2: '#a2d718',
+  },
+  {
+    name: 'SharePoint',
+    desc: 'Enterprise Search',
+    productValue: { current: '8', target: '15' },
+    logo: SharePoint,
+    color1: '#fed243',
+    color2: '#e7b008',
+  },
+];
+
 function App() {
+  const [value, setValue] = useState('current');
+
+  const handleChange = () =>
+    setValue(value === 'target' ? 'current' : 'target');
+
   return (
     <ChakraProvider theme={MyNewTheme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <Product variant="primary" />
-          <Button variant="primary">hi</Button>
-        </Grid>
-      </Box>
+      <VStack height="100vh" justifyContent="center" spacing="75px">
+        <HStack>
+          {data.map(({ name, desc, productValue, logo, color1, color2 }) => (
+            <Product
+              value={productValue[value]}
+              name={name}
+              desc={desc}
+              logo={logo}
+              color1={color1}
+              color2={color2}
+            ></Product>
+          ))}
+        </HStack>
+
+        <FormControl display="flex" alignItems="center" justifyContent="center">
+          <FormLabel mx="12px" my="0" textAlign="right" width="200px">
+            Current
+          </FormLabel>
+          <Switch
+            colorScheme="cyan"
+            size="lg"
+            id="email-alerts"
+            onChange={handleChange}
+            isChecked={value === 'target'}
+          />
+          <FormLabel mx="12px" my="0" textAlign="left" width="200px">
+            Target
+          </FormLabel>
+        </FormControl>
+      </VStack>
     </ChakraProvider>
   );
 }
--- src/components/product/product.js
+++ src/components/product/product.js
@@ -1,29 +1,93 @@
 import React from 'react';
-import { Circle, VStack, Image, Tag } from '@chakra-ui/react';
-import Outlook from '../../images/icons/outlook.svg';
+import Chart from 'react-apexcharts';
+import { Circle, Tag, Image, VStack } from '@chakra-ui/react';
 
-function Product() {
+const Product = ({ value, name, desc, logo, color1, color2 }) => {
   return (
-    <Circle
-      justifyContent="center"
-      alignItems="center"
-      flexDirection="column"
-      backgroundColor="lightgrey"
-      h="250px"
-      w="250px"
-      spacing="30px"
+    <div
+      style={{
+        position: 'relative',
+        width: '280px',
+      }}
     >
-      <VStack spacing="10px">
-        <Image w="80px" src={Outlook} alt="Outlook" marginLeft="-12px"></Image>
-        <Tag size="lg" variant="solid" colorScheme="blackAlpha">
-          Outlook
-        </Tag>
-        <Tag size="sm" variant="solid">
-          Email & Calendar
-        </Tag>
-      </VStack>
-    </Circle>
+      <Chart
+        type="radialBar"
+        series={[value]}
+        options={{
+          labels: [name],
+          fill: {
+            colors: [color1],
+            type: 'gradient',
+            gradient: {
+              gradientToColors: [color2],
+              stops: [0, 100],
+            },
+          },
+          chart: {
+            background: 'transparent',
+            animations: {
+              speed: 500,
+              dynamicAnimation: {
+                speed: 150,
+              },
+            },
+          },
+          grid: {
+            padding: {
+              top: -30,
+              bottom: -30,
+              left: -30,
+              right: -30,
+            },
+          },
+          plotOptions: {
+            radialBar: {
+              startAngle: -90,
+              endAngle: 270,
+              hollow: {
+                size: '60%',
+              },
+            },
+          },
+        }}
+        width="280"
+      />
+      <Circle
+        style={{
+          background: 'rgb(255,255,255)',
+          background:
+            'linear-gradient(135deg, rgba(255,255,255,1) 0%, rgba(211,211,211,1) 100%)',
+          position: 'absolute',
+          top: 'calc(50% - 1px)',
+          left: 'calc(50% + 1px)',
+          transform: 'translateX(-50%) translateY(-50%)',
+        }}
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="column"
+        h="180px"
+        w="180px"
+      >
+        <VStack spacing="6px">
+          <Image w="70px" src={logo} alt={name}></Image>
+          <Tag
+            size="md"
+            variant="solid"
+            style={{ color: 'grey', background: 'white' }}
+          >
+            <b>{name}</b>
+          </Tag>
+          <Tag
+            size="sm"
+            variant="solid"
+            style={{ color: 'grey', background: 'white', fontSize: '10px' }}
+          >
+            {desc}
+          </Tag>
+        </VStack>
+      </Circle>
+    </div>
   );
-}
+};
 
 export default Product;
